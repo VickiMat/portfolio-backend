88017503d68c8b7ab28b128eb67743161120b7ba
vicki <vickimathurin94@gmail.com>
2022-04-28 16:21:24 -0300

    mi primer commit, creacion del proyecto

---
+++ src/main/java/com/portfoliosb/MiBackEnd/MiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliosb.MiBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfoliosb/MiBackEnd/MiBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliosb.MiBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
